b273d8f395e1249c91842207a5150bc1fce0a63d
iKamranShahzad <mailkamranshahzad@gmail.com>
2024-10-11 19:05:30 +0500

    Added Favicon & Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import "./index.css";
+
 function App() {
   return (
-    <>
-      <div className="text-black">Hello, Welcome to BlinkSend</div>
-    </>
+    <div className="animated-background">
+      <header className="flex gap-1 flex-col items-center justify-center relative z-10">
+        <img className="mt-2 w-1/5" src="/Logo.webp" alt="BlinkSend Logo"></img>
+        <p className="text-lg mt-1">
+          Instant, Secure, and Limitless File Sharing at the Blink of an Eye
+        </p>
+      </header>
+    </div>
   );
 }
 
