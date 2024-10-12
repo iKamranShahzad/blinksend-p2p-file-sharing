ad14392a190b684a622c706eba4052dbeae96781
iKamranShahzad <mailkamranshahzad@gmail.com>
2024-10-12 22:42:30 +0500

    Added Background Animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="animated-background">
+    <div className="gradient-background">
       <header className="flex gap-1 flex-col items-center justify-center relative z-10">
         <img className="mt-2 w-1/5" src="/Logo.webp" alt="BlinkSend Logo"></img>
         <p className="text-lg mt-1">
